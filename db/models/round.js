6e833289349ebf9cb20f7583693959e807eb5fcc
Robert Hunter Jr <robert_hunterjr@utexas.edu>
2017-10-24 11:41:08 -0500

    Added inProgress boolean to Round schema

--- db/models/round.js
+++ db/models/round.js
@@ -6,6 +6,7 @@ const roundSchema = new mongoose.Schema({
   round_number: Number,
   players: [{type: sObject, ref: 'player'}],
   completed: Boolean,
+  in_progress: Boolean,
   course: {type: sObject, ref: 'course'},
   date: Date,
   cards: {type: sObject, ref: 'card'},
